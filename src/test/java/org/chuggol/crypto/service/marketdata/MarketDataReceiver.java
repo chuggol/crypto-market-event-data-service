8251f6d685e25828acc8e30cc335435ed137d3a7
chuggol <chuggold@gmail.com>
2017-09-18 22:05:55 -0400

    Test receive messages

---
+++ src/test/java/org/chuggol/crypto/service/marketdata/MarketDataReceiver.java
@@ -0,0 +1,33 @@
+package org.chuggol.crypto.service.marketdata;
+
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+import com.google.cloud.pubsub.v1.MessageReceiver;
+import com.google.cloud.pubsub.v1.Subscriber;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.PubsubMessage;
+import com.google.pubsub.v1.SubscriptionName;
+import com.google.pubsub.v1.TopicName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MarketDataReceiver implements ApplicationRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(MarketDataReceiver.class);
+
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        TopicName topicName = TopicName.create("crypto-175617", "inbound-trades");
+        SubscriptionName subscriptionName = SubscriptionName.create("crypto-175617", "inbound-trades-to-market-event-data-service");
+        Subscriber subscriber = Subscriber.defaultBuilder(subscriptionName, new MessageReceiver() {
+            @Override
+            public void receiveMessage(PubsubMessage pubsubMessage, AckReplyConsumer ackReplyConsumer) {
+                LOG.info("Received: {}", pubsubMessage.getData().toString());
+                ackReplyConsumer.ack();
+            }
+        }).build();
+
+    }
+}
